9d897494b0b0d0f76a29791355aa4887f4a0d724
atharv-sarode12git config --global user.email electronicscomputer00@gmail.comgit config --global user.name atharv-sarode12 <electronicscomputer00@gmail.com>
2024-10-17 15:51:06 +0530

    todays work

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello atharv")
+const user = {
+    username:"Atharv",
+    price:999,
+    welcomeMsg:function(){
+        console.log(`${this.username},Welcome to Site`);
+    }
+}
+
+// user.welcomeMsg()
+
+// user.username = "honey"
+
+// console.log(this)
+
+
+// const chai = function(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+console.log(user.price);
